297754129f972dec59ad467a348979f4b0d0e989
MichaelThai <huy.thai@ymail.com>
2019-12-26 00:12:34 +0700

    add more image refactor start screen UI

--- my_store/lib/src/views/screens/start-screen/start.screen.dart
+++ my_store/lib/src/views/screens/start-screen/start.screen.dart
@@ -1,45 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:my_store/src/constants/image.constant.dart';
-import 'package:my_store/src/constants/styling.constant.dart';
-import 'package:my_store/src/routes/routes-path.dart';
-import 'package:my_store/src/shared/widgets/round-button.widget.dart';
 
 class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       body: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(bottom: 60.0),
-                child: Image.asset(ImageConstant.START_SCREEN_LOGO),
+        child: Stack(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/tea-7.jpg'),
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(Colors.black26, BlendMode.darken),
+
+                ),
               ),
-              Text(
-                'Welcome!',
-                textAlign: TextAlign.center,
-                style: StylingConstant.kStartScreenHeader,
-              ),
-              RoundedButton(
-                buttonLabel: 'Sign in',
-                onPress: () {
-                  Navigator.pushNamed(context, RoutingPath.loginRoute);
-                },
-              ),
-              RoundedButton(
-                buttonLabel: 'Sign Up',
-                onPress: () {},
-                isOutlineButton: true,
-              ),
-            ],
-          ),
+            ),
+//            Container(
+//              color: Color.fromRGBO(0, 0, 0, 0.4),
+//            ),
+          ],
         ),
       ),
     );
+
+//    return Scaffold(
+//      resizeToAvoidBottomInset: false,
+//      body: Container(
+//        child: Padding(
+//          padding: const EdgeInsets.all(30.0),
+//          child: Column(
+//            crossAxisAlignment: CrossAxisAlignment.stretch,
+//            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//            children: <Widget>[
+//              Container(
+//                margin: EdgeInsets.only(bottom: 60.0),
+//                child: Image.asset(ImageConstant.START_SCREEN_LOGO),
+//              ),
+//              Text(
+//                'Welcome!',
+//                textAlign: TextAlign.center,
+//                style: StylingConstant.kStartScreenHeader,
+//              ),
+//              RoundedButton(
+//                buttonLabel: 'Sign in',
+//                onPress: () {
+//                  Navigator.pushNamed(context, RoutingPath.loginRoute);
+//                },
+//              ),
+//              RoundedButton(
+//                buttonLabel: 'Sign Up',
+//                onPress: () {},
+//                isOutlineButton: true,
+//              ),
+//            ],
+//          ),
+//        ),
+//      ),
+//    );
   }
 }
