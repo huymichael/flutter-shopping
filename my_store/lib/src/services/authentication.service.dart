83cc1d42ea05d183a1837f5da0be64b70e279e81
Michael Thai <>
2020-01-31 17:59:12 +0700

    add firebase

---
+++ my_store/lib/src/services/authentication.service.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+
+class AuthenticationService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future signUpWithEmail({@required String email, @required String password}) {
+    return null;
+  }
+
+  Future loginWithEmail({@required String email, @required String password}) {
+    return null;
+  }
+}
